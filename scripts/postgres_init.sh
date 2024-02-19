c1576d06d3165b50e652f10cd845e4c3bfae698e
Hernan Vignolo <hernan.vignolo@emilabs.ai>
2024-02-18 21:15:26 -0300

    fix(docker): add missing volume mount and adjust init script for postgres

--- scripts/postgres_init.sh
+++ scripts/postgres_init.sh
@@ -2,7 +2,7 @@
 
 psql \
     -v ON_ERROR_STOP=1 \
-    --username "${POSTGRES_USER}" \
+    --username "postgres" \
     --dbname "${POSTGRES_DB}" <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
 EOSQL
